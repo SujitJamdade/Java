ac96b6dd32b19e5c0f2b72e8cea9dbd307009200
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2021-01-19 23:00:56 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,26 @@
+package basic_data;
+
+public class Array {
+
+	public static void main (String args[]) {
+		
+		int a[] = {10, 20, 30, 40, 50, 80, 60, 15};
+		
+		System.out.println(a[5]);
+	}
+
+}
+// output : 80
+
+/**
+
+Array : Array is a collection of a number having similar kind of information stored into continuous 
+memory location.
+
+int a[] = {10, 20, 56, 71, 45}
+
+lengt method :
+
+a.length
+
+*/
